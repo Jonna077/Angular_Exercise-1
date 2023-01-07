e3ef2ecbccb17a4eea0c7bc18c3d5618e5bef648
Jonna077 <jonnamaemayang7@gmail.com>
2023-01-08 00:45:04 +0800

    FinalCommit

--- index.js
+++ index.js
@@ -1,150 +1,105 @@
 "use strict";
+//Jonna Mae Mayang
+//Exercise 1
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.displayPopulation = exports.addPopulation = exports.displayCity = exports.addCity = exports.displayCountries = exports.addCountries = exports.test = void 0;
+exports.converting = exports.movingToZero = exports.ISBN10 = exports.addCountries = exports.test = void 0;
 const test = () => {
-    console.log("Hello to the world");
+    console.log("Outputs");
 };
 exports.test = test;
 (0, exports.test)();
-var countries = [];
-const addCountries = (country) => {
-    countries.push(country);
-    (0, exports.displayCountries)();
-};
+class addCountries {
+    constructor(city, country, population) {
+        this.city = city;
+        this.country = country;
+        this.population = population;
+    }
+}
 exports.addCountries = addCountries;
-const displayCountries = () => {
-    var listCountries = document.getElementById('myList');
-    if (listCountries) {
-        while (listCountries.hasChildNodes() && listCountries.firstChild) {
-            listCountries.removeChild(listCountries.firstChild);
-        }
+var countries = JSON.parse(localStorage.getItem("countries") || "[]");
+function addCountry(country) {
+    countries.push(country);
+    localStorage.setItem("country", JSON.stringify(countries));
+}
+function displayCountries(countryList = countries) {
+    let list = document.getElementById("mylist");
+    list.innerHTML = "";
+    for (let countrys of countryList) {
+        let li = document.createElement("li");
+        li.textContent = `${countrys.city}, ${countrys.country}, ${countrys.population}`;
+        list.appendChild(li);
     }
-    countries.forEach(data => {
-        let ul = document.createElement('ul');
-        ul.innerText = data;
-        if (listCountries) {
-            listCountries.appendChild(ul);
-        }
+}
+let countryForm = document.getElementById("countryForm");
+countryForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let city = document.getElementById("city").value;
+    let country = document.getElementById("country").value;
+    let population = document.getElementById("population").valueAsNumber;
+    addCountry(new addCountries(city, country, population));
+    displayCountries(countries);
+});
+let searchInput = document.getElementById("searchCountry");
+searchInput.addEventListener("input", () => {
+    let searchCountries = searchInput.value.toLowerCase();
+    let filterCountries = countries.filter((countriess) => {
+        return (countriess.city.toLowerCase().includes(searchCountries) || countriess.country.toLowerCase().includes(searchCountries));
     });
+    displayCountries(filterCountries);
+});
+//2
+const ISBN10 = () => {
+    let isbn = "1112223339X";
+    if ((isbn))
+        console.log(isbn + " -> True");
+    else
+        console.log(isbn + " -> False");
 };
-exports.displayCountries = displayCountries;
-const addCity = (city) => {
-    countries.push(city);
-    (0, exports.displayCity)();
-};
-exports.addCity = addCity;
-const displayCity = () => {
-    var listCity = document.getElementById('myList');
-    if (listCity) {
-        while (listCity.hasChildNodes() && listCity.firstChild) {
-            listCity.removeChild(listCity.firstChild);
+exports.ISBN10 = ISBN10;
+(0, exports.ISBN10)();
+//3
+const movingToZero = () => {
+    function moveZerosToEnd(arr) {
+        for (let i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] === 0) {
+                arr.push(0);
+                arr.slice(i, 1);
+            }
         }
+        return arr;
     }
-    countries.forEach(data => {
-        let ul = document.createElement('ul');
-        ul.innerText = data;
-        if (listCity) {
-            listCity.appendChild(ul);
-        }
-    });
+    console.log(moveZerosToEnd([false, 1, 0, 1, 2, 0, 1, 3, 'a']));
 };
-exports.displayCity = displayCity;
-// var population:bigint[] = []
-const addPopulation = (populations) => {
-    countries.push(populations);
-    (0, exports.displayPopulation)();
+exports.movingToZero = movingToZero;
+(0, exports.movingToZero)();
+//4
+const converting = () => {
+    function Firstconvert(s) {
+        return s.split(' ').map(word => word.split('').map(letter => {
+            if (letter === 'z')
+                return 'A';
+            if (letter === '0')
+                return '0';
+            let x = parseInt(letter) ? letter : String.fromCharCode(letter.charCodeAt(letter.length - 1) + 1);
+            if (/([aeiou])/g.test(x))
+                return x.toLowerCase();
+            return x;
+        }).join('')).join(' ');
+    }
+    function Secondconvert(s) {
+        s = s.toLowerCase();
+        return s.split(' ').map(word => word.split('').map(letter => {
+            if (letter === 'z')
+                return 'A';
+            if (letter === '0')
+                return '0';
+            let x = parseInt(letter) ? letter : String.fromCharCode(letter.charCodeAt(letter.length - 1) + 1);
+            if (/([aeiou])/g.test(x))
+                return x.toUpperCase();
+            return x;
+        }).join('')).join(' ');
+    }
+    console.log("Change it up! from Cat30 -> " + Firstconvert('Cat30') + " -> " + Secondconvert('Cat30'));
 };
-exports.addPopulation = addPopulation;
-const displayPopulation = () => {
-    var listPopulation = document.getElementById('myList');
-    if (listPopulation) {
-        while (listPopulation.hasChildNodes() && listPopulation.firstChild) {
-            listPopulation.removeChild(listPopulation.firstChild);
-        }
-    }
-    countries.forEach(data => {
-        let ul = document.createElement('ul');
-        ul.innerHTML = (data).toString();
-        if (listPopulation) {
-            listPopulation.appendChild(ul);
-        }
-    });
-};
-exports.displayPopulation = displayPopulation;
-
-function ISBN10(isbn) {
-    var len = isbn.length;
-    
-    if (len !== 10) {
-        return false;
-    }
-    
-    return isbn.split('')
-        .map(function (num, index) {
-           return (num === 'X' && index === len - 1 ? 10 : parseInt(num, 10)) * (index + 1); 
-        })
-        .reduceRight(function (a, b) {
-            return a + b;
-        }, 0) % 11 === 0;
-  }
-   
-  let isbn = "1112223339X";
-
-  if (ISBN10(isbn))
-   console.log(isbn+" -> True");
-  else
-  console.log(isbn+" -> False");
-
-function moveZerosToEnd(arr) {
-    for(let i= arr.length-1; i>=0; i--){
-       if(arr[i]===0){
-          arr.push(0) 
-          arr.splice(i,1)
-       }
-    }
-    return arr
- }
- console.log(moveZerosToEnd([false, 1, 0, 1, 2, 0, 1, 3, 'a']))
-
-
-
-        function Firstconvert(s) {
-            
-            
-            return s.split(' ').map(word=>
-                
-                word.split('').map(letter=>{
-                    if(letter === 'z' ) return 'A'
-                    if(letter === '0')  return '0'
-                    let x = parseInt(letter) ? letter : String.fromCharCode(letter.charCodeAt(letter.length - 1) + 1)
-                    if(/([aeiou])/g.test(x)) return x.toLowerCase()
-                    
-                    return x
-                   
-            }).join('')
-            
-            ).join(' ')
-          }
-        
-
-       
-        function Secondconvert(s) {
-            s= s.toLowerCase()
-            
-            return s.split(' ').map(word=>
-                
-                word.split('').map(letter=>{
-                    if(letter === 'z' ) return 'A'
-                    if(letter === '0')  return '0'
-                    let x = parseInt(letter) ? letter : String.fromCharCode(letter.charCodeAt(letter.length - 1) + 1)
-                    if(/([aeiou])/g.test(x)) return x.toUpperCase()
-                    return x
-                   
-            }).join('')
-            
-            ).join(' ')
-          }
-          console.log("Change it up! from Cat30 -> "+Firstconvert('Cat30')+" -> "+Secondconvert('Cat30'))
-
-
-    
+exports.converting = converting;
+(0, exports.converting)();
--- index.ts
+++ index.ts
@@ -1,96 +1,138 @@
+//Jonna Mae Mayang
+//Exercise 1
+
 export const test = () => {
-    console.log("Hello to the world")
+    console.log("Outputs")
 }
 
 test();
 
-var countries:any[] = []
-
-
-export const addCountries = (country: string) => {
-    countries.push(country)
-    displayCountries()
-    
+interface CountriesInfos {
+    city: string;
+    country: string;
+    population: number;
 }
 
+export class addCountries implements CountriesInfos {
+    city: string;
+    country: string;
+    population: number;
+    constructor(city: string, country: string, population: number) {
+        this.city = city;
+        this.country = country;
+        this.population = population;
+    }
+}
 
-export const displayCountries = () => {
-    var listCountries = document.getElementById('myList')
+var countries: addCountries[] = JSON.parse(localStorage.getItem("countries") || "[]");
 
-    if(listCountries){
-        while(listCountries.hasChildNodes() && listCountries.firstChild){
-            listCountries.removeChild(listCountries.firstChild)
+function addCountry(country: addCountries) {
+    countries.push(country);
+    localStorage.setItem("country", JSON.stringify(countries));
+}
+
+function displayCountries(countryList: addCountries[] = countries) {
+    let list = document.getElementById("mylist") as HTMLElement;
+    list.innerHTML = "";
+    for (let countrys of countryList) {
+        let li = document.createElement("li");
+        li.textContent = `${countrys.city}, ${countrys.country}, ${countrys.population}`;
+        list.appendChild(li);
+    }
+}
+
+let countryForm = document.getElementById("countryForm") as HTMLElement;
+countryForm.addEventListener("submit", (event: Event) => {
+    event.preventDefault();
+    let city = (document.getElementById("city") as HTMLInputElement).value;
+    let country = (document.getElementById("country") as HTMLInputElement).value;
+    let population = (document.getElementById("population") as HTMLInputElement).valueAsNumber;
+    addCountry(new addCountries(city, country, population));
+    displayCountries(countries);
+    
+});
+
+let searchInput = document.getElementById("searchCountry") as HTMLInputElement;
+searchInput.addEventListener("input", () => {
+    let searchCountries = searchInput.value.toLowerCase();
+    let filterCountries = countries.filter((countriess) => {
+        return (countriess.city.toLowerCase().includes(searchCountries) ||countriess.country.toLowerCase().includes(searchCountries));
+    });
+    displayCountries(filterCountries);
+});
+
+
+//2
+export const ISBN10 = () => {
+
+    let isbn = "1112223339X";
+
+    if ((isbn))
+        console.log(isbn + " -> True");
+    else
+        console.log(isbn + " -> False");
+}
+ISBN10()
+
+
+
+//3
+export const movingToZero = () => {
+    function moveZerosToEnd(arr: (string | number | boolean)[]) {
+        for (let i = arr.length - 1; i >= 0; i--) {
+            if (arr[i] === 0) {
+                arr.push(0)
+                arr.slice(i, 1)
+            }
         }
+        return arr
+    }
+    console.log(moveZerosToEnd([false, 1, 0, 1, 2, 0, 1, 3, 'a']))
+}
+
+movingToZero()
+
+
+
+//4
+export const converting = () => {
+    function Firstconvert(s: string) {
+
+
+        return s.split(' ').map(word =>
+
+            word.split('').map(letter => {
+                if (letter === 'z') return 'A'
+                if (letter === '0') return '0'
+                let x = parseInt(letter) ? letter : String.fromCharCode(letter.charCodeAt(letter.length - 1) + 1)
+                if (/([aeiou])/g.test(x)) return x.toLowerCase()
+
+                return x
+
+            }).join('')
+
+        ).join(' ')
     }
 
 
-    countries.forEach(data => {
-        let ul = document.createElement('ul')
-        ul.innerText = data
 
-        if(listCountries){
-            listCountries.appendChild(ul)
-        }
-    })
+    function Secondconvert(s: string) {
+        s = s.toLowerCase()
 
-}
+        return s.split(' ').map(word =>
 
-export const addCity = (city: string) => {
-    countries.push(city)
-    displayCity()
-    
-}
+            word.split('').map(letter => {
+                if (letter === 'z') return 'A'
+                if (letter === '0') return '0'
+                let x = parseInt(letter) ? letter : String.fromCharCode(letter.charCodeAt(letter.length - 1) + 1)
+                if (/([aeiou])/g.test(x)) return x.toUpperCase()
+                return x
 
+            }).join('')
 
-export const displayCity = () => {
-    var listCity = document.getElementById('myList')
-
-    if(listCity){
-        while(listCity.hasChildNodes() && listCity.firstChild){
-            listCity.removeChild(listCity.firstChild)
-        }
+        ).join(' ')
     }
-
-
-    countries.forEach(data => {
-        let ul = document.createElement('ul')
-        ul.innerText = data
-
-        if(listCity){
-            listCity.appendChild(ul)
-        }
-    })
+    console.log("Change it up! from Cat30 -> " + Firstconvert('Cat30') + " -> " + Secondconvert('Cat30'))
 
 }
-
-
-export const addPopulation = (populations: bigint) => {
-    countries.push(populations)
-    displayPopulation()
-    
-}
-
-
-export const displayPopulation = () => {
-    var listPopulation = document.getElementById('myList')
-
-    if(listPopulation){
-        while(listPopulation.hasChildNodes() && listPopulation.firstChild){
-            listPopulation.removeChild(listPopulation.firstChild)
-        }
-    }
-
-
-    countries.forEach(data => {
-        let ul = document.createElement('ul')
-        ul.innerHTML  = (data).toString()
-
-        if(listPopulation){
-            listPopulation.appendChild(ul)
-        }
-
-       
-    })
-
-}
-
+converting()
